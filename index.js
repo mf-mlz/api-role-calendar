a3f3000ede0f97adc91f2309b4f6b7afb9c3b4a1
“Fernanda <maryfermtzlb@gmail.com>
2024-08-20 15:28:21 -0600

    feat(Index Api): 7 days for last sunday => New Roles Month + 1

--- index.js
+++ index.js
@@ -27,24 +27,39 @@ app.use(express.json());
 /* Day => Month => Year */
 const currentDate = new Date();
 const currentMonth = currentDate.getMonth();
-const month = currentMonth + 1;
+let month = currentMonth + 1;
 const currentYear = currentDate.getFullYear();
-const numberOfDaysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+
+const lastSundayOfMonth = getLastSundayOfMonth(currentYear, currentDate.getMonth());
+/* Num. Days for last sunday of the month */
+const daysUntilLastSunday = lastSundayOfMonth - currentDate.getDate();
+/* Si faltan menos o igual a 7 días => Agarramos el mes siguiente */
+month = daysUntilLastSunday <= 7 ? month + 1 : month;
+
+
+
+const numberOfDaysInMonth = new Date(currentYear, month, 0).getDate();
 const sundaysInMonth = [];
 const dateCurrently = new Date().toISOString().split('.')[0];
 
-
 for (let day = 1; day <= numberOfDaysInMonth; day++) {
-    const date = new Date(currentYear, currentMonth, day);
+    const date = new Date(currentYear, month - 1, day);
     if (date.getDay() === 0) {
         sundaysInMonth.push(day);
     }
 }
 
-/* Last Day => Count */
-const lastIndexDatMonth = sundaysInMonth.length - 1;
-const lastDatMonth = sundaysInMonth[lastIndexDatMonth];
-const daysUntilEndOfMonth = numberOfDaysInMonth - lastDatMonth;
+
+/* Function Get Last Sunday of Month */
+function getLastSundayOfMonth(year, month) {
+
+    const lastDayOfMonth = new Date(year, month + 1, 0);
+    const lastDayOfWeek = lastDayOfMonth.getDay();
+    const lastSunday = new Date(lastDayOfMonth);
+    lastSunday.setDate(lastDayOfMonth.getDate() - lastDayOfWeek);
+
+    return lastSunday.getDate();
+}
 
 /* Module API REST [GET] */
 
@@ -104,12 +119,14 @@ async function generateRoles(res, nameRol, nameUser){
         /* Por seguridad que no se dupliquen los datos, verificamos si en la Tabla roles  ya exiten registros les año y mes actual, de ser así, ya no ejectamos ninguna inserción  */
             const sqlVerify = 'SELECT * FROM `'+ nameRol +'` WHERE month = ? AND year = ?';
             const [rowsVerify, fieldsVerify] = await db.execute(sqlVerify, [month, currentYear]);
-    
+            
+            
             if (rowsVerify.length === 0) {
                 const sql = 'SELECT * FROM '+ nameUser +' WHERE status = 1';
                 const [rows, fields] = await db.execute(sql);
+                
                 const randomUsers = [];
-    
+                
                 if (rows.length < sundaysInMonth.length) {
                     const userFaltante = parseInt(sundaysInMonth.length) - parseInt(rows.length);
                     for (let index = 0; index < userFaltante; index++) {
@@ -118,20 +135,22 @@ async function generateRoles(res, nameRol, nameUser){
                         rows.push(userRandomExtra);
                     }
                 }
-    
-                /* Obtenemos 4 Usuarios al azar para formar nuestro arreglo al azar, borrando el usuario Seleccionado para evitar duplicar */
+
+                
+                /* Obtenemos (n) Usuarios al azar para formar nuestro arreglo al azar, borrando el usuario Seleccionado para evitar duplicar */
                 for (let index = 0; index < sundaysInMonth.length; index++) {
                     const randomIndex = random(0, rows.length - 1);
                     const selectedUser = rows.splice(randomIndex, 1)[0];
                     randomUsers.push(selectedUser);
     
                 }
-    
+
+                
                 /* Los insertamos en la BD en la Tabla [roles]  */
                 let count = 0;
                 for (const user of randomUsers) {
                     const insertSql = 'INSERT INTO '+ nameRol +' (name_user, day, month, year, created_at) VALUES (?, ?, ?, ?, ?)';
-                    await db.execute(insertSql, [user.name, sundaysInMonth[count], currentMonth + 1, currentYear, dateCurrently]);
+                    await db.execute(insertSql, [user.name, sundaysInMonth[count], month, currentYear, dateCurrently]);
                     count++;
                 }
     
